00bec8122a0eff995a34ad18e629ccf8c46a1f21
Rahul Mondal <64986704+rahulmondalrm@users.noreply.github.com>
2020-06-07 22:14:10 +0530

    Add files via upload

---
+++ python (operators).py
@@ -0,0 +1,111 @@
+Python 3.8.3 (tags/v3.8.3:6f8c832, May 13 2020, 22:20:19) [MSC v.1925 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 50+10
+60
+>>> 60-50
+10
+>>> -60+50
+-10
+>>> 50/3
+16.666666666666668
+>>> 50//3
+16
+>>> 50.0/3.0
+16.666666666666668
+>>> 50.0/2.5
+20.0
+>>> 50.0/10
+5.0
+>>> 50/10
+5.0
+>>> 50//10
+5
+>>> 50*10
+500
+>>> -70+20
+-50
+>>> 10%3
+1
+>>> 21/5
+4.2
+>>> 21%5
+1
+>>> 2**5
+32
+>>> 10**10
+10000000000
+>>> 5+2+5+6+3
+21
+>>> 5+9-6*10
+-46
+>>> 5+9-6*10/20
+11.0
+>>> (5+9)-6*(10/20)
+11.0
+>>> 60*20/10
+120.0
+>>> 60*20/30
+40.0
+>>> # Variable
+>>> myInt=10
+>>> myFloat=10.5
+>>> myfloat=2.5
+>>> myComplex=5j
+>>> myNum= 10e2
+>>> myNum
+1000.0
+>>> 10**2
+100
+>>> myNum=2e5
+>>> myNum
+200000.0
+>>> myNum=10e10
+>>> myNum
+100000000000.0
+>>> myNum=10E10
+>>> myNum
+100000000000.0
+>>> myString='Rahul'
+>>> mystring
+Traceback (most recent call last):
+  File "<pyshell#38>", line 1, in <module>
+    mystring
+NameError: name 'mystring' is not defined
+>>> myString
+'Rahul'
+>>> myString='Rahul'
+>>> myString
+'Rahul'
+>>> myFloat
+10.5
+>>> myFloat = myInt
+>>> myFloat
+10
+>>> myFloat = float(myInt)
+>>> myFloat
+10.0
+>>> myInt
+10
+>>> myInt = myFloat
+>>> myInt
+10.0
+>>> myInt = int(myFloat)
+>>> myInt
+10
+>>> type(myInt)
+<class 'int'>
+>>> type(myFloat)
+<class 'float'>
+>>> type(myNum)
+<class 'float'>
+>>> type(myString)
+<class 'str'>
+>>> myInt
+10
+>>> myFloat
+10.0
+>>> myInt + myFloat
+20.0
+>>> type(myInt + myFloat)
+<class 'float'>
+>>> 
